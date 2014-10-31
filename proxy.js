b940f35a7fb604d20d701bd40fc673f021f5ae6a
Shawn Inder <shawn@vigour.io>
2014-10-31 02:54:28 -0400

    Rename index.js to proxy.js

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ var httpProxy = require('http-proxy')
 	, from = process.argv[2]
 	, to = process.argv[3]
 if (!from || !to) {
-	return console.error("Usage: node index.js <from> <to>\n"
+	return console.error("Usage: node proxy.js <from> <to>\n"
 		+ "  `from` and `to` are port numbers"
 		+ "  don't forget `sudo` if `from` is 80")
 }

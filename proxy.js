02a9bd8b17230b53d57504f03cd994819bf00520
Shawn Inder <shawn@vigour.io>
2014-12-01 15:48:42 +0100

    Add error listener

--- proxy.js
+++ proxy.js
@@ -1,14 +1,21 @@
 var httpProxy = require('http-proxy')
 	, from = process.argv[2]
 	, to = process.argv[3]
+	, server
+
 if (!from || !to) {
 	return console.error("Usage: node proxy.js <from> <to>\n"
 		+ "  `from` and `to` are port numbers"
 		+ "  don't forget `sudo` if `from` is 80")
 }
-httpProxy.createProxyServer({
+
+server = httpProxy.createProxyServer({
 	target: 'http://localhost:' + to
-}).listen(from)
+})
+server.on('error', function (err) {
+	console.error("Oops", err)
+})
+server.listen(from)
 console.log("Forwarding port "
 	+ from
 	+ " to port "

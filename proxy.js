0f089c16e889949e534fbdb1786fcff813017a55
Shawn Inder <shawn@vigour.io>
2015-02-25 16:34:54 +0100

    Allow redirecting https to http

--- proxy.js
+++ proxy.js
@@ -1,22 +1,33 @@
 var httpProxy = require('http-proxy')
+	, fs = require('fs')
 	, from = process.argv[2]
 	, to = process.argv[3]
+	, options = {}
 	, server
 
 if (!from || !to) {
 	return console.error("Usage: node proxy.js <from> <to>\n"
-		+ "  `from` and `to` are port numbers"
+		+ "  `from` and `to` are port numbers\n"
 		+ "  don't forget `sudo` if `from` is 80")
 }
 
-server = httpProxy.createProxyServer({
-	target: 'http://localhost:' + to
-})
+if (from === '443') {
+	options.ssl = {
+		key: fs.readFileSync('cert/server.key')
+		, cert: fs.readFileSync('cert/server.crt')
+	}
+	options.secure = false
+}
+options.target = 'http://'
+
+options.target += 'localhost:' + to
+
+server = httpProxy.createProxyServer(options)
 server.on('error', function (err) {
 	console.error("Oops", err)
 })
 server.listen(from)
 console.log("Forwarding port "
 	+ from
-	+ " to port "
+	+ " to "
 	+ to)
